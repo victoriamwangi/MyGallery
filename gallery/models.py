005f7c6eeda56243a592206846df346c6cf6a007
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-27 14:59:39 +0300

    feat: adding images with name, description and post date

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,7 @@ class Image(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length= 40)
     image = models.ImageField(upload_to = 'articles/', null=True, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     
     def save_image(self):
         self.save()
