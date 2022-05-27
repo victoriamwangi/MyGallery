efa7ab2949986adcbb6a3bfc2f0ce4cd584637f2
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-27 14:20:48 +0300

    add image model

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,14 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length= 40)
+    image = models.ImageField(upload_to = 'articles/', null=True, blank=True)
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.name
     
