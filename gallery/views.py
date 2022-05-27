36bbf29b4df8f42b83178f548f8bc518483df00a
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-27 11:47:37 +0300

    remove env folder

--- gallery/views.py
+++ gallery/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    # title= 'hehe'
-    return render(request, 'index.html')
+  
+    return render(request, 'index.html' )
