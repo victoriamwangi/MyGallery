5ca256b0a0a9ed29ed1b010b105af6524d905f2e
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-27 16:01:47 +0300

    feat:upload  images with name, description and post date

--- gallery/models.py
+++ gallery/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import datetime as dt
 
 
 class Image(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length= 40)
-    image = models.ImageField(upload_to = 'articles/', null=True, blank=True)
+    image = models.ImageField(upload_to = 'images/', null=True, blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     
     def save_image(self):
@@ -12,6 +13,11 @@ class Image(models.Model):
         
     def delete_image(self):
         self.delete()
+    
+    @classmethod
+    def all_images(cls):
+        images= Image.objects.all()
+        return images
     
     def __str__(self):
         return self.name
--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 
 # Create your views here.
-def home(request):
-  
-    return render(request, 'index.html' )
+def home(request):   
+    images = Image.all_images()
+    return render(request, 'index.html', {"images": images}  )
